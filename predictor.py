121dd5ae7ad96399c7e89461ddf95b67926836f2
liolinv-sudo <liolinv@gmail.com>
2026-07-03 22:36:20 +0200

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -11,3 +11,18 @@ def expected_score(home_elo, away_elo):
             (away_elo - adjusted_home) / 400
         )
     )
+
+def probabilities(home_elo, away_elo):
+
+    home_win = expected_score(home_elo, away_elo)
+
+    draw = 0.25
+
+    home_win *= 0.75
+    away_win = (1 - home_win - draw)
+
+    return {
+        "home": round(home_win, 3),
+        "draw": round(draw, 3),
+        "away": round(away_win, 3)
+    }
