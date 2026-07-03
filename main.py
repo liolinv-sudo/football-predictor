2394fb50a4c0cef871ab073a26b7d0c2b85e5948
liolinv-sudo <liolinv@gmail.com>
2026-07-03 21:38:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ API_KEY = os.getenv("API_KEY")
 
 ODDS_API_KEY = os.getenv("ODDS_API_KEY")
 
+def fetch_odds():
+
+    url = "https://api.the-odds-api.com/v4/sports/soccer/odds/"
+
+    params = {
+        "apiKey": ODDS_API_KEY,
+        "regions": "eu",
+        "markets": "h2h"
+    }
+
+    response = requests.get(url, params=params)
+    return response.json()
+
 headers = {
     "X-Auth-Token": API_KEY
 }
