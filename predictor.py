0cce3c9b87195743bae3881a59317c0649e980d5
liolinv-sudo <liolinv@gmail.com>
2026-07-03 22:34:49 +0200

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,10 +1,13 @@
 import math
 
+HOME_ADVANTAGE = 75
+
 def expected_score(home_elo, away_elo):
 
+    adjusted_home = home_elo + HOME_ADVANTAGE
+
     return 1 / (
-        1 + math.pow(
-            10,
-            (away_elo - home_elo) / 400
+        1 + 10 ** (
+            (away_elo - adjusted_home) / 400
         )
     )
