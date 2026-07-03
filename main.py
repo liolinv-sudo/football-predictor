5b5052d6216ec2eafe1600749a020a7aee359d91
liolinv-sudo <liolinv@gmail.com>
2026-07-03 20:29:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+matches = [
+    {
+        "home": "Malmö FF",
+        "away": "AIK",
+        "home_win": 45,
+        "draw": 28,
+        "away_win": 27
+    },
+    {
+        "home": "Hammarby",
+        "away": "Djurgården",
+        "home_win": 39,
+        "draw": 30,
+        "away_win": 31
+    }
+]
+
 @app.get("/")
 def root():
-    return {"message": "Football Predictor is running"}
+    return {"message": "Football Predictor"}
+
+@app.get("/matches")
+def get_matches():
+    return matches
