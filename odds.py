82da19ec845ba16bdbf09a58893b85ed0cf1161c
liolinv-sudo <liolinv@gmail.com>
2026-07-04 04:32:48 +0200

    Update odds.py

--- odds.py
+++ odds.py
@@ -16,3 +16,17 @@ def fetch_odds():
 
     r = requests.get(url, params=params)
     return r.json()
+
+def get_match_odds(home, away, odds_data):
+
+    for game in odds_data:
+
+        if game["home_team"] == home and game["away_team"] == away:
+
+            outcomes = game["bookmakers"][0]["markets"][0]["outcomes"]
+
+            return {
+                o["name"]: o["price"] for o in outcomes
+            }
+
+    return None
