11943bd00a37b60cb6c46fdf08a5bb684e5711b6
liolinv-sudo <liolinv@gmail.com>
2026-07-04 05:40:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,31 @@
 import os
 import requests
 from fastapi import FastAPI
-from predictor import probabilities
-from odds import fetch_odds, get_match_odds
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+from predictor import get_probabilities, calculate_ev, kelly
+from odds import fetch_odds, get_match_odds
+
+app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-app = FastAPI()
+
+API_KEY = os.getenv("API_KEY")
+ODDS_API_KEY = os.getenv("ODDS_API_KEY")
+
 
 @app.get("/")
 def root():
     return FileResponse("frontend/index.html")
 
-API_KEY = os.getenv("API_KEY")
 
-ODDS_API_KEY = os.getenv("ODDS_API_KEY")
-
-def fetch_odds():
-
-    url = "https://api.the-odds-api.com/v4/sports/soccer/odds/"
-
-    params = {
-        "apiKey": ODDS_API_KEY,
-        "regions": "eu",
-        "markets": "h2h"
-    }
-
-    response = requests.get(url, params=params)
-    return response.json()
-
-def get_best_odds(match, odds_data):
-
-    for game in odds_data:
-
-        if (game["home_team"] == match["homeTeam"]["name"] and
-            game["away_team"] == match["awayTeam"]["name"]):
-
-            outcomes = game["bookmakers"][0]["markets"][0]["outcomes"]
-
-            return {
-                outcome["name"]: outcome["price"]
-                for outcome in outcomes
-            }
-
-    return None
-
-
-
-
-headers = {
-    "X-Auth-Token": API_KEY
-}
-
-#@app.get("/matches")
-#def get_matches():
-
-  @app.get("/matches")
+@app.get("/matches")
 def get_matches():
 
     matches = requests.get(
         "https://api.football-data.org/v4/matches",
-        headers={"X-Auth-Token": os.getenv("API_KEY")}
+        headers={"X-Auth-Token": API_KEY}
     ).json()["matches"]
 
     odds_data = fetch_odds()
@@ -81,32 +45,25 @@ def get_matches():
 
         ev = calculate_ev(probs["home"], odds[home])
 
-        # 🔥 FILTER: visa bara +EV
         if ev > 0:
 
-kelly_value = kelly(probs["home"], odds[home])
-result.append({
-    "home": home,
-    "away": away,
-    "ev": round(ev, 3),
-    "odds": odds,
-    "kelly": kelly_value   # <-- viktigt
-})
-            
+            kelly_value = kelly(probs["home"], odds[home])
 
-    # 🔥 SORTERA bästa först
+            result.append({
+                "home": home,
+                "away": away,
+                "ev": round(ev, 3),
+                "odds": odds,
+                "kelly": round(kelly_value, 3)
+            })
+
     result.sort(key=lambda x: x["ev"], reverse=True)
 
     return result
 
-def calculate_ev(probability, odds):
-    return (probability * odds) - 1
 
 app.mount(
     "/frontend",
     StaticFiles(directory=os.path.join(BASE_DIR, "frontend")),
     name="frontend"
 )
-
-
-
