291300547d3497754e2a843b8523116dfdf64569
liolinv-sudo <liolinv@gmail.com>
2026-07-10 18:41:42 +0200

    Update odds.py

--- odds.py
+++ odds.py
@@ -37,6 +37,44 @@ def normalize(name):
     return name.lower().strip()
 
 
+# def get_match_odds(home, away, odds_data):
+
+  #  home_n = normalize(home)
+   # away_n = normalize(away)
+
+ #   for game in odds_data:
+
+    #    game_home = normalize(game.get("home_team", ""))
+     #   game_away = normalize(game.get("away_team", ""))
+
+        # flexibel matchning
+    #    if (home_n == game_home and away_n == game_away) or \
+        #   (home_n == game_away and away_n == game_home):
+
+         #   outcomes = game["bookmakers"][0]["markets"][0]["outcomes"]
+
+           # odds_map = {
+          #      normalize(o["name"]): o["price"]
+           #     for o in outcomes
+           # }
+
+          #  odds_map = {}
+
+           #    for o in outcomes:
+
+           #    if normalize(o["name"]) == game_home:
+            #   odds_map["home"] = o["price"]
+
+           #    elif normalize(o["name"]) == game_away:
+           #    odds_map["away"] = o["price"]
+
+            #   else:
+           #    odds_map["draw"] = o["price"]
+
+           #    return odds_map
+
+  #  return None
+
 def get_match_odds(home, away, odds_data):
 
     home_n = normalize(home)
@@ -47,30 +85,29 @@ def get_match_odds(home, away, odds_data):
         game_home = normalize(game.get("home_team", ""))
         game_away = normalize(game.get("away_team", ""))
 
-        # flexibel matchning
-        if (home_n == game_home and away_n == game_away) or \
-           (home_n == game_away and away_n == game_home):
+        if (
+            (home_n == game_home and away_n == game_away)
+            or
+            (home_n == game_away and away_n == game_home)
+        ):
 
             outcomes = game["bookmakers"][0]["markets"][0]["outcomes"]
 
-           # odds_map = {
-          #      normalize(o["name"]): o["price"]
-           #     for o in outcomes
-           # }
-
             odds_map = {}
 
-               for o in outcomes:
+            for o in outcomes:
 
-               if normalize(o["name"]) == game_home:
-               odds_map["home"] = o["price"]
+                name = normalize(o["name"])
 
-               elif normalize(o["name"]) == game_away:
-               odds_map["away"] = o["price"]
+                if name == game_home:
+                    odds_map["home"] = o["price"]
 
-               else:
-               odds_map["draw"] = o["price"]
+                elif name == game_away:
+                    odds_map["away"] = o["price"]
 
-               return odds_map
+                else:
+                    odds_map["draw"] = o["price"]
+
+            return odds_map
 
     return None
